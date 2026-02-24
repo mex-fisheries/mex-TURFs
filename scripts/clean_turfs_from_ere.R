d54a198b28fb0b7b5f481544b2a8fd717ad0d5cf
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2026-02-24 13:27:40 -0500

    Add script and processed data to GitHub

---
+++ scripts/clean_turfs_from_ere.R
@@ -0,0 +1,46 @@
+################################################################################
+# title
+################################################################################
+#
+# Your Name Here
+# Your email here
+# date
+#
+# Description
+#
+################################################################################
+
+# SET UP #######################################################################
+
+## Load packages ---------------------------------------------------------------
+library(here)
+library(tidyverse)
+library(janitor)
+library(sf)
+
+## Load data -------------------------------------------------------------------
+turfs_raw <- read_rds(here("data/raw/TURFcoopsMX.Rda"))
+  
+
+# PROCESSING ###################################################################
+
+## Some step -------------------------------------------------------------------
+turfs_clean <- turfs_raw |>
+  clean_names() |> 
+  select(state, turf_id, coop, contains("especie"),
+         eu_rnpa = clave_de_la_unidad_economica,
+         n_species = speciesnum) |> 
+  drop_na(turf_id) |> # Ere says NAs are not actual TURFs
+  st_as_sf() |> 
+  st_transform(crs = "EPSG:4326")
+
+# EXPORT #######################################################################
+
+
+## The final step --------------------------------------------------------------  
+write_sf(obj = turfs_clean,
+         dsn = here("data/processed/mex_turfs.gpkg"),
+         delete_dsn = T)
+write_rds(x = turfs_clean |> 
+            st_drop_geometry(),
+         file = here("data/processed/mex_turfs.rds"))
